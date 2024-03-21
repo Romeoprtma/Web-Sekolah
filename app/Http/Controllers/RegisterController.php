5b7e213d434e0a1971c4d51c63e9fbe3560f6a6d
Taufiq Reza <romeomuliap@gmail.com>
2024-03-21 09:17:28 +0700

    update database

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Symfony\Contracts\Service\Attribute\Required;
+use App\Models\User;
 
 class RegisterController extends Controller
 {
@@ -11,12 +11,17 @@ class RegisterController extends Controller
         return view('register.index');
     }
 
-    public function store(request $request) {
-       $request->validate([
-        'name' => 'required|max:225',
-        'password' => 'required|min:5|max:255'
-       ]);
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'username' => 'required|max:255',  // Corrected max length
+            'password' => 'required|min:5|max:255',
+            'password2' => 'required|min:5|max:255|same:password'
+        ]);
 
+        User::create($validatedData);
 
+        // Redirect to a success page or send a success notification
+        return redirect('/login')->with('success', 'Registrasi berhasil!');
     }
 }
--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -7,16 +7,31 @@
       <img src="img/logo-tut.png" alt="Logo">
     </div>
     <div class="w-full px-16 md:w-1/2 md:px-8"> <h2 class="text-3xl font-bold text-center">Register</h2>
-      <form action="register" class="flex flex-col gap-4" method="post">
+      <form action="/register" class="flex flex-col gap-4" method="post">
         @csrf
         <label for="username" class="mt-8">Username</label>
-        <input type="text" name="username" id="username" class="p-2 border rounded-xl" placeholder="Masukkan Username">
+        <input type="text" name="username" id="username" class="p-2 border rounded-xl @error('username') is-invalid @enderror" value="{{old('username')}}" placeholder="Masukkan Username" required>
+        @error('username')
+        <div class="invalid-feedback">
+            {{ $message }}
+        </div>
+        @enderror
 
         <label for="password">Password</label>
-        <input type="password" name="password" id="password" class="p-2 border rounded-xl" placeholder="Masukkan Password">
+        <input type="password" name="password" id="password" class="p-2 border rounded-xl @error('password') is-invalid @enderror" value="{{old('username')}}" placeholder="Masukkan Password" required>
+        @error('password')
+        <div class="invalid-feedback">
+            {{ $message }}
+        </div>
+        @enderror
 
         <label for="password2">Ulang Password</label>
-        <input type="password" name="password2" id="password2" class="p-2 border rounded-xl" placeholder="Masukkan Ulang Password">
+        <input type="password" name="password2" id="password2" class="p-2 border rounded-xl @error('password') is-invalid @enderror" value="{{old('username')}}" placeholder="Masukkan Ulang Password" required>
+        @error('password')
+        <div class="invalid-feedback">
+            {{ $message }}
+        </div>
+        @enderror
 
         <button type="submit" class="py-2 text-black bg-yellow-400 hover:bg-yellow-500 rounded-xl">
           Register
